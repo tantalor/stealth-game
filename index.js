ea3802b0990cf22eea5e9c96148132d7f8b82e9e
John Tantalo <john.tantalo@gmail.com>
2013-08-12 09:22:36 -0700

    made mobile friendly

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 function StealthGame(canvas) {
   this.canvas_ = canvas;
   
-  this.width_ = this.canvas_.width = 1024;
-  this.height_ = this.canvas_.height = this.width_ * 3 / 4;
+  var clientWidth = document.documentElement.clientWidth;
+  var clientHeight = document.documentElement.clientHeight;
+  
+  var maxWidth = 1024;
+  var maxHeight = maxWidth * 3 / 4;
+  
+  this.width_ = this.canvas_.width = Math.min(clientWidth, maxWidth);
+  this.height_ = this.canvas_.height = Math.min(clientHeight, maxHeight);
+  
+  this.canvas_.style.marginTop = (clientHeight - this.height_) / 2 + 'px';
   
   this.context_ = this.canvas_.getContext('2d');
-  this.context_.strokeStyle = 'black';
-  this.context_.lineWidth = 1;
-  this.context_.strokeRect(0, 0, this.width_, this.height_);
   
   startFrames(this);
 }
 
 StealthGame.prototype.drawFrame = function(t) {
-  this.context_.clearRect(1, 1, this.width_ - 2, this.height_ - 2);
+  this.context_.clearRect(0, 0, this.width_, this.height_);
   
   // Model coordinates
   var x = Math.sin(t / 1000);
@@ -22,12 +27,13 @@ StealthGame.prototype.drawFrame = function(t) {
   // Screen coordinates.
   var cx = this.width_ / 2;
   var cy = this.height_ / 2;
-  var r = cx / 3;
+  var r = Math.min(cx, cy);
   var dx = x * r, dy = -y * r;
   
+  this.context_.strokeStyle = 'black';
   this.context_.lineWidth = 3;
   this.context_.beginPath();
-  this.context_.moveTo(cx, cy);
+  this.context_.moveTo(cx - dx, cy - dy);
   this.context_.lineTo(cx + dx, cy + dy);
   this.context_.stroke();
 };
