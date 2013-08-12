1775917dc6730b7d9c21d41e56f7766eabdc80e8
John Tantalo <john.tantalo@gmail.com>
2013-08-11 22:30:16 -0700

    add animation

--- index.js
+++ index.js
@@ -8,4 +8,35 @@ function StealthGame(canvas) {
   this.context_.strokeStyle = 'black';
   this.context_.lineWidth = 1;
   this.context_.strokeRect(0, 0, this.width_, this.height_);
+  
+  startFrames(this);
+}
+
+StealthGame.prototype.drawFrame = function(t) {
+  this.context_.clearRect(1, 1, this.width_ - 2, this.height_ - 2);
+  
+  // Model coordinates
+  var x = Math.sin(t / 1000);
+  var y = Math.cos(t / 1000);
+  
+  // Screen coordinates.
+  var cx = this.width_ / 2;
+  var cy = this.height_ / 2;
+  var r = cx / 3;
+  var dx = x * r, dy = -y * r;
+  
+  this.context_.lineWidth = 3;
+  this.context_.beginPath();
+  this.context_.moveTo(cx, cy);
+  this.context_.lineTo(cx + dx, cy + dy);
+  this.context_.stroke();
 };
+
+
+function startFrames(obj) {
+  var runFrame = function(t) {
+    obj.drawFrame(t);
+    window.requestAnimationFrame(runFrame);
+  };
+  runFrame();
+}
