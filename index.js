3dc4d80aa87eb31bbab7decd9e1352ed1771dc98
John Tantalo <john.tantalo@gmail.com>
2013-08-11 22:17:49 -0700

    draw game border

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function StealthGame(gameDiv) {
-  this.gameDiv_ = gameDiv;
+function StealthGame(canvas) {
+  this.canvas_ = canvas;
+  
+  this.width_ = this.canvas_.width = 1024;
+  this.height_ = this.canvas_.height = this.width_ * 3 / 4;
+  
+  this.context_ = this.canvas_.getContext('2d');
+  this.context_.strokeStyle = 'black';
+  this.context_.lineWidth = 1;
+  this.context_.strokeRect(0, 0, this.width_, this.height_);
 };
