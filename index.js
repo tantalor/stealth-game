c288eb1393f10310335c81143bec0b1adb3f19b1
John Tantalo <john.tantalo@gmail.com>
2013-08-12 20:43:42 -0700

    Agent in model coordinates

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ StealthGame = function(canvas) {
   
   this.context_ = this.canvas_.getContext('2d');
   
+  this.scale_ = Math.min(this.width_ / 2, this.height_ / 2);
+  
   this.agent_ = new StealthGame.Agent(0, 0);
   
   startFrames(this);
@@ -26,16 +28,11 @@ StealthGame.prototype.drawFrame = function(t) {
   // Model coordinates
   var x = Math.sin(t / 2000);
   var y = Math.cos(t / 2000);
-  
-  // Screen coordinates.
-  var cx = this.width_ / 2;
-  var cy = this.height_ / 2;
-  var r = Math.min(cx, cy);
-  var dx = x * r, dy = -y * r;
 
   this.context_.save();
   this.context_.translate(this.width_ / 2, this.height_ / 2);
-  this.agent_.moveTo(dx, dy);
+  this.context_.scale(this.scale_, this.scale_);
+  this.agent_.moveTo(x, -y);
   this.agent_.drawFrame(this.context_);
   this.context_.restore();
 };
@@ -44,7 +41,7 @@ StealthGame.prototype.drawFrame = function(t) {
 StealthGame.Agent = function(x, y) {
   this.x = x;
   this.y = y;
-  this.r = 10;
+  this.r = .05;
 };
 
 
@@ -56,9 +53,9 @@ StealthGame.Agent.prototype.moveTo = function (x, y) {
 StealthGame.Agent.prototype.drawFrame = function(context) {
   context.beginPath();
   context.arc(this.x, this.y, this.r, 0, 6.284);
-  context.fillStyle = '#aaf';
+  context.fillStyle = 'blue';
   context.fill();
-  context.lineWidth = 1;
+  context.lineWidth = this.r / 8;
   context.strokeStyle = 'black';
   context.stroke();
 };
